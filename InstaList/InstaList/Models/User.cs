24d7676f4bb26773d496993dd1a4b1cc3adc1b82
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-12-08 18:06:00 +0100

    Dodaj pliki projektów.

---
+++ InstaList/InstaList/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaList.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ InstaList/InstaList/NaviList.xaml.cs
@@ -0,0 +1,63 @@
+﻿using InstaList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InstaList
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NaviList : ContentPage
+    {
+        public NaviList()
+        {
+            InitializeComponent();
+            list.ItemsSource = new List<User>()
+            {
+                new User()
+                {
+                 
+                 Name="Marcin",
+                 Description="szef xyz",
+                 ImageUrl="https://lorempixel.com/100/100/people/1/"
+                 
+                },
+                 new User()
+                {
+
+                 Name="Franek",
+                 Description="mniejszy szef xyz",
+                 ImageUrl="https://lorempixel.com/100/100/people/2/"
+                },
+
+                  new User()
+                {
+
+                 Name="Łucja",
+                 Description="zastępca szefa xyz",
+                 ImageUrl="https://lorempixel.com/100/100/people/3/"
+                },
+
+                  new User()
+                  {
+                      Name="Iwona",
+                      Description = "common user",
+                       ImageUrl="https://lorempixel.com/100/100/people/4/"
+                  }
+            };
+        }
+
+        async private void list_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+            var user=e.SelectedItem as User;
+            await Navigation.PushAsync(new DetailPage(user));
+            list.SelectedItem = null;
+        }
+    }
+}
